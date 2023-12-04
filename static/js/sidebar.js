a7ac692a2d382b12d4de2f52f2c3058ef5804a82
Bryan <35320836+potentpwnables@users.noreply.github.com>
2023-12-04 11:12:03 -0500

    adding dynamic toggling of sidebar menu

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,5 +1,8 @@
-document.getElementById("avatar").addEventListener("click", () => {
+function toggleSidebarMenu() {
     var menu = document.getElementById("menu");
+    if (typeof menu === 'undefined' || menu === null) {
+        return;
+    }
     if (menu.style.visibility === "hidden" || menu.style.visibility === "") {
         menu.style.visibility = "visible";
         menu.style.opacity = 1;
@@ -7,4 +10,4 @@ document.getElementById("avatar").addEventListener("click", () => {
         menu.style.visibility = "hidden";
         menu.style.opacity = 0;
     }
-})
+}
