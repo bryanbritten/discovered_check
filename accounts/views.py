2241a93bafe062a4ee846b52d7832cd0cfc77047
Bryan <85406498+bryanbritten@users.noreply.github.com>
2024-09-19 16:05:44 -0400

    adds the skeleton for handling authentication of users

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    print('logging out the user')
+    return redirect('accounts:login')
+
+def signup(request):
+    return render(request, 'accounts/signup.html')
+
+def profile(request):
+    return render(request, 'accounts/profile.html')
