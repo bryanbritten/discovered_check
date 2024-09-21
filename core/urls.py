e1ae9dc23ff2cf4dc7f9ac152910ef054e086170
Bryan <85406498+bryanbritten@users.noreply.github.com>
2024-09-21 13:36:33 -0400

    refactors URL paths for dashboards

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('dashboards.urls')),
     path('accounts/', include('accounts.urls')),
-    path('dashboards/', include('dashboards.urls')),
     path('admin/', admin.site.urls),
 ]
--- dashboards/urls.py
+++ dashboards/urls.py
@@ -3,7 +3,7 @@ from dashboards import views
 
 app_name = 'dashboards'
 urlpatterns = [
-    path('', views.redirect_to_overview, name='redirect_to_overview'),
+    path('', views.root, name='root'),
     path('overview/', views.overview, name='overview'),
     path('time/', views.time, name='time'),
     path('tactics/', views.tactics, name='tactics'),
