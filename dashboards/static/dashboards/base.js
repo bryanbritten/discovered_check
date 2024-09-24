738dc03663e65e130a0296d4844d319c34d546f3
Bryan <85406498+bryanbritten@users.noreply.github.com>
2024-09-24 14:25:12 -0400

    adds the basic functionality of a dropdown user menu

---
+++ dashboards/static/dashboards/base.js
@@ -0,0 +1,5 @@
+function toggleUserMenu() {
+    document.getElementById('menu').classList.toggle('hidden');
+    document.getElementById('username-icon').classList.toggle('fa-caret-down');
+    document.getElementById('username-icon').classList.toggle('fa-caret-up');
+};
